986b4afb9d043f9f4d8c357310e96c4c57d64757
Andrew Ash <andrewash9@gmail.com>
2016-02-29 18:30:22 -0500

    wrote review model

---
+++ server/db/models/review.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var User = require('./user');
+var Product = require('./product');
+
+var schema = new mongoose.Schema({
+    content: {
+        type: String,
+        minlength: 15
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: User
+    },
+    product: {
+        type: mongoose.Schema.ObjectId,
+        ref: Product
+    }
+});
+
+mongoose.model('Review', schema);
