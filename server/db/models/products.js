7025c913918f7693123c2af7d46e09ea3ce6c69c
johnquiwa <jcq211@nyu.edu>
2016-02-29 19:06:50 -0500

    created productSchema

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 var _ = require('lodash');
 
 var productSchema = new mongoose.Schema({
-
+	title: {type: String, required: true},
+	description: {type: String, required: true},
+	img: {type: String} //how will we serve this image up? 
 });
 
 
