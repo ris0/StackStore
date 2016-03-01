61304459e53393a50e6674a6ae5810f633cece10
Andrew Ash <andrewash9@gmail.com>
2016-03-01 11:06:47 -0500

    refactored to avoid typing mongoose.model.modelName over and over

--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -1,16 +1,17 @@
 'use strict';
 var router = require('express').Router();
 var mongoose = require('mongoose');
+var Review = mongoose.model.Review;
 module.exports = router;
 
 router.get('/reviews/:productId', function (req, res, next) {
-    mongoose.model.Review.find({ 'product': req.product._id })
+    Review.find({ 'product': req.product._id })
     .then(res.json)
     .then(null, next);
 });
 
 router.post('/reviews/:productId', function (req, res, next) {
-    mongoose.model.Review.create({ req.body })
+    Review.create({ req.body })
     .then(res.status(201).json)
     .then(null, next);
 });
