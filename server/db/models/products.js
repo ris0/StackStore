0c6ff48b39fe5da6cea22204957c2a6c75e695df
Rich Song <richasong@gmail.com>
2016-02-29 19:33:24 -0500

    Completed productSchema definition

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -3,10 +3,23 @@ var mongoose = require('mongoose');
 var _ = require('lodash');
 
 var productSchema = new mongoose.Schema({
-	title: {type: String, required: true},
-	description: {type: String, required: true},
-	img: {type: String} //how will we serve this image up? 
+
+    title: {
+        type: String,
+        required: true
+    },
+	description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+    image: String
+
 });
 
 
-mongoose.model('Product', schema);
+module.exports = mongoose.model('Product', productSchema);
