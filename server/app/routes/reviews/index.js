72813891d2575aaf43b0f80e3ad5569edf23d988
Andrew Ash <andrewash9@gmail.com>
2016-03-02 09:25:59 -0500

    Reconciled routing paths with Product router. Tests still nonfunctional (waiting for User model)

--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -4,13 +4,13 @@ var mongoose = require('mongoose');
 var Review = mongoose.model.Review;
 module.exports = router;
 
-router.get('/reviews/:productId', function (req, res, next) {
+router.get('/:productId', function (req, res, next) {
     Review.find({ 'product': req.product._id })
     .then(res.json)
     .then(null, next);
 });
 
-router.post('/reviews/:productId', function (req, res, next) {
+router.post('/:productId', function (req, res, next) {
     Review.create({ req.body })
     .then(res.status(201).json)
     .then(null, next);
