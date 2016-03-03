07b57d0d134d3ce63ec96d2df2289a3de7f37ab9
Andrew Ash <andrewash9@gmail.com>
2016-03-03 17:43:48 -0500

    cart factory

---
+++ browser/js/cart/cart.factory.js
@@ -0,0 +1,78 @@
+app.factory('CartFactory', function ($http) {
+
+    var CartFactory = {};
+
+    CartFactory.getAllCarts = function () {
+        return $http.get('/api/cart')
+            .then(carts => carts.data)
+            .catch(function (err) {
+                console.error(err);
+            });
+    };
+
+    CartFactory.getCurrentCart = function (data) {
+        return $http.get('/api/cart/current', data)
+        .then(cart => cart.data)
+        .catch(function (err) {
+            console.error(err);
+        });
+    };
+
+    CartFactory.getPastCarts = function (data) {
+        return $http.get('/api/cart/past', data)
+        .then(carts => carts.data)
+        .catch(function (err) {
+            console.error(err);
+        });
+    };
+
+    CartFactory.getCartById = function (cartId) {
+        return $http.get('/api/cart/' + cartId)
+            .then(cart => cart.data)
+            .catch(function (err) {
+                console.error(err);
+            });
+    };
+
+    CartFactory.createCart = function (data) {
+        return $http.post('/api/cart', data)
+            .then(cart => cart.data)
+            .catch(function (err) {
+                console.error(err);
+            });
+    };
+
+    CartFactory.addProduct = function (productId, quantity) {
+        return $http.post('/api/cart' + productId + '/' + quantity)
+            .then(cart => cart.data)
+            .catch(function (err) {
+                console.error(err);
+            });
+    };
+
+    CartFactory.updateProduct = function (productId, quantity) {
+        return $http.put('/api/cart/' + productId + '/' + quantity)
+            .then(cart => cart.data)
+            .catch(function (err) {
+                console.error(err);
+            });
+    };
+
+    CartFactory.clearCurrentCart = function (userId) {
+        return $http.delete('/clear-cart/' + userId)
+            .then(null, function (err) {
+                console.error(err);
+            });
+    };
+
+    CartFactory.deleteProduct = function (productId) {
+        return $http.delete('/api/cart/' + productId)
+            .then(cart => cart.data)
+            .catch(function (err) {
+                console.error(err);
+            });
+    };
+
+    return CartFactory;
+
+});
--- browser/js/users/users.factory.js
+++ browser/js/users/users.factory.js
@@ -3,23 +3,32 @@ app.factory('UsersFactory', function ($http) {
     var UsersFactory = {};
 
     UsersFactory.getAllUsers = function () {
-        return $http.get('/api/users').then(users => users.data);
+        return $http.get('/api/users')
+        .then(users => users.data)
+        .catch(function(err) { console.error(err); });
     };
 
     UsersFactory.getUserById = function (userId) {
-        return $http.get('/api/users/' + userId).then(user => user.data);
+        return $http.get('/api/users/' + userId)
+        .then(user => user.data)
+        .catch(function(err) { console.error(err); });
     };
 
     UsersFactory.createUser = function (data) {
-        return $http.post('/api/users', data).then(user => user.data);
+        return $http.post('/api/users', data)
+        .then(user => user.data)
+        .catch(function(err) { console.error(err); });
     };
 
     UsersFactory.updateUser = function (userId, data) {
-        return $http.put('/api/users/' + userId, data).then(user => user.data);
+        return $http.put('/api/users/' + userId, data)
+        .then(user => user.data)
+        .catch(function(err) { console.error(err); });
     };
 
     UsersFactory.deleteUser = function (userId) {
-        return $http.put('/api/users/' + userId);
+        return $http.put('/api/users/' + userId)
+        .catch(function(err) { console.error(err); });
     };
 
     return UsersFactory;
