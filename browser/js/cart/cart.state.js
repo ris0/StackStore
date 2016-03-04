a8150d72f7854d35a61379361aa521ad1b0a94ad
Jai Kamat <jaipkamat@gmail.com>
2016-03-04 16:19:54 -0500

    working on cartFactory resolves

--- browser/js/cart/cart.state.js
+++ browser/js/cart/cart.state.js
@@ -3,5 +3,43 @@ app.config(function ($stateProvider) {
     $stateProvider.state('cart', {
         url: '/cart',
         templateUrl: 'js/cart/cart.html',
+        resolve: {
+           allCarts: function (CartFactory) {
+               return CartFactory.getAllCarts();
+           },
+
+           oneCart: function (CartFactory, data) {
+               return CartFactory.getCurrentCart(data);
+           },
+
+           pastCarts: function (CartFactory, data) {
+               return CartFactory.getPastCarts(data)
+           },
+
+           cartById: function (CartFactory, $stateParams) {
+               // TODO: Are we passing in the right data?
+               return CartFactory.getCartById($stateParams.cartId);
+           },
+
+           makeCart: function (CartFactory, data) {
+               return CartFactory.createCart(data);
+           },
+
+           addProduct: function (CartFactory, $stateParams) {
+               // TODO: Are we passing in the right data?
+               return CartFactory.addProduct($stateParms.productId, quantity, data);
+           },
+
+           updateProduct: function (CartFactory, $stateParms) {
+               return CartFactory.updateProduct($stateParams.userId)
+           },
+
+           clearCurrentCart: function (CartFactory, $stateParams) {
+               return CartFactory.clearCurrentCart($stateParams.userId)
+           },
+
+           deleteProduct: function (CartFactory) {
+               return CartFactory.deleteProduct()
+           }
     });
 });
