33f47b8c9ba9c6d1c3513442385bb1df45973bce
Jai Kamat <jaipkamat@gmail.com>
2016-03-04 16:26:02 -0500

    minor edits

--- browser/js/cart/cart.state.js
+++ browser/js/cart/cart.state.js
@@ -12,10 +12,6 @@ app.config(function ($stateProvider) {
                return CartFactory.getCurrentCart(data);
            },
 
-           pastCarts: function (CartFactory, data) {
-               return CartFactory.getPastCarts(data)
-           },
-
            cartById: function (CartFactory, $stateParams) {
                // TODO: Are we passing in the right data?
                return CartFactory.getCartById($stateParams.cartId);
@@ -24,22 +20,5 @@ app.config(function ($stateProvider) {
            makeCart: function (CartFactory, data) {
                return CartFactory.createCart(data);
            },
-
-           addProduct: function (CartFactory, $stateParams) {
-               // TODO: Are we passing in the right data?
-               return CartFactory.addProduct($stateParms.productId, quantity, data);
-           },
-
-           updateProduct: function (CartFactory, $stateParms) {
-               return CartFactory.updateProduct($stateParams.userId)
-           },
-
-           clearCurrentCart: function (CartFactory, $stateParams) {
-               return CartFactory.clearCurrentCart($stateParams.userId)
-           },
-
-           deleteProduct: function (CartFactory) {
-               return CartFactory.deleteProduct()
-           }
     });
 });
