17d4d139ac7b37d44be70403f68b67c2de2a47ea
johnquiwa <jcq211@nyu.edu>
2016-03-08 12:29:44 -0500

    edit product form is almost functional

--- browser/js/admin/admin.controller.js
+++ browser/js/admin/admin.controller.js
@@ -1,5 +1,18 @@
 app.controller('AdminCtrl', function ($scope, $log, allProducts, allUsers) {
     $scope.products = allProducts;
     $scope.users = allUsers;
+    $scope.thisProduct;
+    $scope.availability = [true,false]
+    $scope.productIsSelected = false;
+    $scope.setProduct = function(product) {
+    	$scope.productIsSelected = true;
+    	$scope.productToEdit = {};
+    	$scope.productToEdit.title = product.title;
+    	$scope.productToEdit.price = product.price;
+    	$scope.productToEdit.quantity = product.quantity;
+    	$scope.productToEdit.availability = product.availability;
+    	$scope.productToEdit.description = product.description;
+    	$scope.productToEdit.image = product.image;
+    }
 
 });
