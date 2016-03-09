cd6b8ce2af078c3e06ca7ee1bee00200607956b4
Rich Song <richasong@gmail.com>
2016-03-08 22:28:24 -0500

    User management completed, working on Order Management

--- browser/js/admin/admin.controller.js
+++ browser/js/admin/admin.controller.js
@@ -1,13 +1,23 @@
-app.controller('AdminCtrl', function ($scope, $log, allProducts, allUsers, CategoriesFactory, allCategories, ProductsFactory) {
-    $scope.products = allProducts;
-    $scope.users = allUsers;
-    $scope.thisProduct;
+app.controller('AdminCtrl', function ($scope, $log, allProducts, allUsers, allCarts, CategoriesFactory, allCategories, ProductsFactory, UsersFactory, CartFactory) {
+    // products
+	$scope.products = allProducts;
     $scope.productAvailability = [true,false];
     $scope.productIsSelected = false;
 	$scope.productToEdit = {};
 	$scope.categories = allCategories;
 
+	// users
+	$scope.userToEdit = {};
+    $scope.users = allUsers;
+	$scope.userIsSelected = false;
+	$scope.userIsAdmin= [true,false];
 
+	// carts
+	$scope.cartToEdit = {};
+	$scope.carts = allCarts;
+	$scope.cartIsSelected = false;
+
+    // <----  product  ---->
     $scope.setProduct = function(product) {
     	$scope.productToEdit._id = product._id;
     	$scope.productIsSelected = true;
@@ -17,16 +27,69 @@ app.controller('AdminCtrl', function ($scope, $log, allProducts, allUsers, Categ
     	$scope.productToEdit.availability = product.availability;
     	$scope.productToEdit.description = product.description;
     	$scope.productToEdit.image = product.image;
-    }
+    };
 
     $scope.submitEditedProduct = function(product) {
     	console.log(product);
     	ProductsFactory.updateProduct(product._id, product)
-    	.then(function(res){
-    		console.log(res);
-    		console.log($scope.products);
-    		return res.data;
-    	})
-    }
+			.then(function(res){
+				console.log(res);
+				console.log($scope.products);
+				return res.data;
+			})
+    };
+
+	// <----  user  ---->
+	$scope.setUser = function(user) {
+		$scope.userIsSelected = true;
+		$scope.userToEdit._id = user._id;
+		$scope.userToEdit.email = user.email;
+		$scope.userToEdit.password = user.password;
+		$scope.userToEdit.isAdmin = user.isAdmin;
+		$scope.userToEdit = user;
+	};
+
+	$scope.submitEditedUser = function(user) {
+		UsersFactory.updateUser(user._id, user)
+			.then(user => user.data)
+	};
+
+	$scope.deleteUser = function(user) {
+		console.log(user);
+		UsersFactory.deleteUser(user._id)
+			.then(user => user.data)
+	};
+
+	// <---- orders ---->
+	$scope.setCart = function(cart) {
+		$scope.cartIsSelected = true;
+		$scope.cartToEdit._id = cart._id;
+		$scope.cartToEdit.contents.quantity = cart.contents.quantity;
+		$scope.cartToEdit.contents.product = cart.contents.product;
+		$scope.cartToEdit = cart;
+	};
+
+	$scope.submitEditedCart = function(cart) {
+
+	};
+
+	/*
+	 <div>
+	 <h3>Artists</h3>
+	 <div class="panel input-group">
+	 <span class="input-group-addon">Filter</span>
+	 <input type="text" class="form-control" placeholder="by name" ng-model="nameSearch">
+	 </div>
+	 <div class="list-group">
+	 <div class="list-group-item" ng-repeat="artist in artists | filter: { name: nameSearch }">
+	 <a ui-sref="artist({artistId: artist._id})">{{ artist.name }}</a>
+	 </div>
+	 </div>
+	 </div>
+
+	 */
+
+
+
 
 });
